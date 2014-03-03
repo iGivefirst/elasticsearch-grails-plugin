99e704ae6b37c17e7e507660d57c8ca4630888d6
Ryan Kistner <azuisleet@gmail.com>
2014-03-03 03:16:00 -0700

    Updated ES library

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,10 +31,8 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        //runtime "org.elasticsearch:elasticsearch:0.19.9"
-        //runtime "org.elasticsearch:elasticsearch-lang-groovy:1.1.0"
-        runtime "org.elasticsearch:elasticsearch:0.90.2"
-        runtime "org.elasticsearch:elasticsearch-lang-groovy:1.5.0"
+        runtime "org.elasticsearch:elasticsearch:1.0.1"
+        runtime "org.elasticsearch:elasticsearch-lang-groovy:2.0.0.RC1"
     }
     plugins {
       build ':release:2.2.1', ':rest-client-builder:1.0.3', {
